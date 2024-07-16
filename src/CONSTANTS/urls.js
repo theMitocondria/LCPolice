8ce10eaf1fc3ae21d56369e7075faefd747f739a
gautam khatri <buddhgautam777@gmail.com>
2024-07-16 09:26:18 +0530

    changed the baseUrl from localhost to deployed

--- src/CONSTANTS/urls.js
+++ src/CONSTANTS/urls.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://lcpolice-backend.onrender.com/api/v1/'
-export const BASE_URL = 'http://localhost:4000/api/v1/'
+export const BASE_URL = 'https://lcpolice-backend.onrender.com/api/v1/'
+// export const BASE_URL = 'http://localhost:4000/api/v1/'
