97639c9aee4f9f17f72f30395c63e74ed2a62113
gautam khatri <buddhgautam777@gmail.com>
2024-07-20 19:23:41 +0530

    feat:✨reporting window changed

--- src/CONSTANTS/urls.js
+++ src/CONSTANTS/urls.js
@@ -1,2 +1,3 @@
 export const BASE_URL = 'https://lcpolice-backend.onrender.com/api/v1/'
+// export const BASE_URL = 'http://localhost:5000/api/v1/'
 export const curve = 'https://res.cloudinary.com/dejvlvhlp/image/upload/f_auto,q_auto/curve_ouoeuq'
